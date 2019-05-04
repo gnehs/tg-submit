40773e2b4d7febf85a9f22dea9827034ed1f1a5b
gnehs <jayuiop@gmail.com>
2019-05-04 23:04:37 +0800

    add helmet, fix error page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
+const helmet = require("helmet");
 const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -14,6 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
+app.use(helmet());
+app.use(helmet.hidePoweredBy({
+  setTo: 'PHP 4.2.0'
+}))
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
